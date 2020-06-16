aef4d2ad7092e07b10921daaa9c5b7889eb39594
boblandsky <flaminiron13-signups@yahoo.com>
2020-06-15 22:06:52 -0700

    Update sf_covid.py
    Added new layer for map, now selectable with the dropdown menu. Fixed some ordering issues, now tables and dropdown menus should have some kind of order.

--- sf_covid.py
+++ sf_covid.py
@@ -9,8 +9,8 @@ import streamlit as st
 today = pnd.now(tz='US/Pacific').format('MMMM DD, YYYY')
 yest = pnd.yesterday().format('MMMM DD, YYYY')
 
-st.title(f"SF COVID Cases by Census ZIP Code")
-st.header(f'As of {today}')
+st.title(f"COVID-19 Dashboard")
+st.header(f'SF Cumulative Cases As of {today}')
 
 # DATA_URL = ('https://data.sfgov.org/resource/favi-qct6.csv?$$app_token=yh5qaeaJSvJrdOSv77ZnroO2u')
 # DATE_COLUMN = 'data_as_of'
@@ -25,21 +25,14 @@ st.markdown('Data sourced from '
             '[data.sfgov.org](https://data.sfgov.org/COVID-19/COVID-19-Cases-and-Deaths-Summarized-by-Geography/tpyr-dvnc)')
 
 @st.cache
-# def load_data(nrows):
-#     # Caches the data
-#     # Cleans up the dates and ZIP codes
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     data['zip_code'] = data['zip_code'].astype(str)
-#     return data
 def load_data(nrows):
     # Caches the data
     # Cleans up the dates and ZIP codes
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows, date_parser=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     data = data[data['area_type']=='ZCTA']
-    data = data.drop(['area_type', 'multipolygon'], axis=1).rename(columns={'id': 'zip_code'})
-    data = data.reset_index().drop('index', axis=1)
+    data = data.drop(['area_type', 'multipolygon', 'deaths'], axis=1).rename(columns={'id': 'zip_code'})
+    data = data.reset_index(drop=True)
     return data
 
 data = load_data(264)
@@ -76,6 +69,7 @@ zip_data = pd.DataFrame({
     'CaseCount': data['count'],
 })
 zip_data.sort_values(by=['CaseCount'])
+zip_data = zip_data.dropna(subset=['CaseCount'])
 
 zip_rate = pd.DataFrame({
     'Rate': data['rate'],
@@ -92,14 +86,11 @@ map_zip_rate = map_zip_rate.dropna()
 zip_map_data = pd.merge(map_zip_code,map_zip_count, on='ZIPCode')
 zip_rate_data = pd.merge(map_zip_code, map_zip_rate, on='ZIPCode')
 zip_rate_data = zip_rate_data.sort_values(by=['ZIPCode'])
-zip_rate_data = zip_rate_data.reset_index(drop=True)
+zip_rate_data = zip_rate_data.reset_index(drop=True).sort_values(by='ZIPCode')
 zip_rate_dict = zip_rate_data.loc[:,'ZIPCode'].to_dict()
 
 
 
-st.sidebar.info('**Info:** If you don\'t see data for your ZIP Code, '
-        'there are 10 or fewer cases counted for your ZIP Code.'
-        )
 st.sidebar.warning('**Show Raw Data**: '
                     'Due to caching, the raw data may not display correctly. '
                     'The charts and map should be using the data last published '
@@ -108,16 +99,30 @@ st.sidebar.warning('**Show Raw Data**: '
 
 if st.sidebar.checkbox('Show raw SF data'):
     st.subheader('Raw SF data')
+    data = data.rename(columns={
+        'zip_code': 'ZIP Code',
+        'count': 'Case Count',
+        'rate': 'Cases per 10,000 People',
+        'acs_population': 'Population',
+        'last_updated_at': 'Last Updated On'
+    })
+    data = data.sort_values(by=['ZIP Code'])
+    data = data.dropna(subset=['Case Count']).reset_index(drop=True)
     st.write(data)
-    date_updated = data[DATE_COLUMN][0].to_pydatetime().date()
+    data['Last Updated On'] = pd.to_datetime(data['Last Updated On'])
+    date_updated = data['Last Updated On'][0].to_pydatetime().date()
     date_updated = date_updated.strftime('%B %d, %Y')
-    st.info(f'Click on a column name to sort. Last updated {date_updated}')
+    st.info(f'Click on a column name to sort. Last updated {date_updated}. '
+                'Case counts with no values have been dropped.'
+                )
 
 st.subheader('Number of Confirmed SF Cases by ZIP Code')
 
 c = alt.Chart(zip_data).mark_bar().encode(
-    y = alt.Y('ZIPCode:N', sort=alt.EncodingSortField(field='CaseCount', op='max', order='descending')),
-    x = 'CaseCount',
+    y = alt.Y('ZIPCode:N',
+                sort=alt.EncodingSortField(field='CaseCount', op='max', order='descending'),
+                axis=alt.Axis(title='ZIP Code')),
+    x = alt.X('CaseCount', axis=alt.Axis(title='Case Count')),
     tooltip = ['ZIPCode', 'CaseCount']
 )
 
@@ -133,8 +138,20 @@ chart_text = c.mark_text(
 
 st.altair_chart(c,use_container_width=True)
 st.info('Mouse over a bar to see the number of cases. '
-        'To expand the graph, click the fullscreen button on the side of the graph.'
+        'To expand the graph, click the toggle on the side of the graph. '
+        'If your ZIP Code is not shown, fewer than 15 cases have been recorded there.'
 )
+
+find_zip = st.selectbox('Select your ZIP Code.',
+                        zip_rate_data.loc[:,'ZIPCode'])
+
+case_num = 0
+for i, zcode in zip_rate_dict.items():
+    if zcode == find_zip:
+        case_num = i
+
+st.write('For ', find_zip, ', the number of cases per 10k people is',
+        str(zip_rate_data.loc[case_num, 'Rate']), '.')
 
 st.subheader('Map of COVID-19 Cases by ZIP Code')
 
@@ -145,7 +162,8 @@ st.pydeck_chart(pdk.Deck(
          longitude=-122.4,
          zoom=11,
      ),
-     layers=[pdk.Layer(
+     layers=[
+        pdk.Layer(
             "ScatterplotLayer",
             zip_rate_data,
             pickable=True,
@@ -161,24 +179,31 @@ st.pydeck_chart(pdk.Deck(
             get_fill_color=[255, 140, 0],
             get_line_color=[0, 0, 0],
         ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            zip_rate_data[zip_rate_data['ZIPCode']==find_zip],
+            pickable=True,
+            opacity=0.2,
+            stroked=True,
+            filled=True,
+            radius_scale=6,
+            radius_min_pixels=1,
+            radius_max_pixels=100,
+            line_width_min_pixels=1,
+            get_position="[lon, lat]",
+            get_radius="Rate*5",
+            get_fill_color=[0, 0, 255],
+            get_line_color=[0, 0, 0],
+        )
+
      ],
      tooltip={
         'html': 'Number of Cases per 10000 Residents in {ZIPCode}: <br>{Rate}</br>',
      },
  ))
-st.info('Mouse over a circle to see the number of cases per 10,000 people.')
-
-find_zip = st.selectbox('Select your ZIP Code.',
-                        zip_rate_data.loc[:,'ZIPCode'])
-
-case_num = 0
-for i, zcode in zip_rate_dict.items():
-    if zcode == find_zip:
-        case_num = i
-
-st.write('For ', find_zip, ', the number of cases per 10k people is',
-        str(zip_rate_data.loc[case_num, 'Rate']), '.')
-
+st.info('Mouse over a circle to see the number of cases per 10,000 people. '
+        'Click the toggle on the side to expand the map. '
+        )
 
 CA_DATA_URL = 'https://data.chhs.ca.gov/dataset/6882c390-b2d7-4b9a-aefa-2068cee63e47/resource/6cd8d424-dfaa-4bdd-9410-a3d656e1176e/download/covid19data.csv'
 CA_DATE_COL = 'Most Recent Date'
@@ -205,12 +230,12 @@ if st.checkbox('Examine other counties in California?'):
     if st.sidebar.checkbox('Show raw CA data'):
         st.subheader('CA Raw COVID 19 Data')
         st.write(ca_data)
-        st.write('Click on a column to sort.')
+        st.info('Click on a column to sort. Click the toggle on the right side of the table to expand it.')
 
-    st.subheader('California COVID-19 Data')
+    st.header('California COVID-19 Data')
     st.markdown('Data sourced from '
                 '[data.ca.gov](https://data.ca.gov/dataset/california-covid-19-hospital-data-and-case-statistics/resource/5342afa3-0e58-40c0-ba2b-9206c3c5b288)')
-    county = st.multiselect('Pick a county to look at:',
+    county = st.multiselect('Pick one or more counties to look at:',
                           ca_county_list,
                           ['San Francisco', 'Sacramento', 'Santa Clara']
                           )
@@ -218,7 +243,7 @@ if st.checkbox('Examine other counties in California?'):
                                 ca_data_columns)
     ca_subset_columns = ca_data.loc[ca_data['County Name'].isin(county)].copy()
     ca_subset_columns.loc[:, 'Most Recent Date'] = ca_subset_columns.loc[:, 'Most Recent Date'].map(pd.to_datetime)
-    ca_graph_columns = ca_subset_columns.loc[:,('County Name', 'Most Recent Date', ca_columns)]
+    ca_graph_columns = ca_subset_columns.loc[:,('County Name', 'Most Recent Date', ca_columns)].copy()
 
     county_chart = alt.Chart(ca_graph_columns).mark_line(size=5).encode(
         x='Most Recent Date',
@@ -227,12 +252,11 @@ if st.checkbox('Examine other counties in California?'):
         tooltip = ['County Name', 'Most Recent Date', ca_columns],
     ).interactive()
     st.altair_chart(county_chart, use_container_width=True)
-    st.info(f'Mouse over the line to see the exact count. Data last updated on {recent_date}.')
+    st.info('Mouse over the line to see the exact count. '
+            f'Data last updated on {recent_date}. '
+            'Data first captured on April 1, 2020.')
 
     st.markdown('---')
-
-    if st.sidebar.checkbox('Show subsetted CA data'):
-        st.write(ca_subset_columns)
 
 st.sidebar.markdown('Made by Oliver Chen. '
                     'Github repository available [here](https://github.com/boblandsky/Streamlit_tutorial).')
